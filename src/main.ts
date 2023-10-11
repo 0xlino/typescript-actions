47c927e9bb94d1c26f5236a0ec0e888871a7ac0b
Lino <hello@benline.co.uk>
2023-10-11 15:27:14 +0000

    Update typescript actions

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,36 @@
+// import * as core from '@actions/core'
+// import {wait} from './wait'
+
+// async function run(): Promise<void> {
+//   try {
+//     const functionName = 
+//   } catch (error) {
+//     if (error instanceof Error) core.setFailed(error.message)
+//   }
+// }
+
+// run()
 import * as core from '@actions/core'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // get the input
+    const functionName: string = core.getInput('functionName')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    if (functionName === 'benstest') {
+        core.debug('benstest');
+        core.setOutput('time', 'benstest');
+    } else {
+        const ms: string = core.getInput('milliseconds')
+        core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    
+        core.debug(new Date().toTimeString())
+        await wait(parseInt(ms, 10))
+        core.debug(new Date().toTimeString())
+    
+        core.setOutput('time', new Date().toTimeString())
+    }    
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
